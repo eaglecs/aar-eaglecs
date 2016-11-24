6551f42e09c22645b55c19ba1958dffd7dac96c4
leducanh <leducanh.bkit10@gmail.com>
2016-11-24 17:53:00 +0700

    update README

---
+++ demolibrary/src/main/java/com/demolib/demolib/Def.java
@@ -0,0 +1,32 @@
+package com.demolib.demolib;
+
+/**
+ * Created by leducanh on 24/11/2016.
+ */
+
+public class Def {
+    //spend
+    public static final int COFFEE = 0;
+    public static final int SHOPPING = 1;
+    public static final int RESTAURANT = 2;
+    public static final int FRIEND = 3;
+
+    // REVENUES
+
+    public static final int SALARY = 4;
+    public static final int BONUS = 5;
+    public static final int GIFT = 6;
+    public static final int PROFIT = 7;
+
+    // TYPE EVENT
+
+    public static final int REVENUE = 1;
+    public static final int SPEND = 2;
+    public static final int DEBT = 3;
+    public static final int ALL = 4;
+
+
+    // Intent
+
+    public static final String STATUS_TRANSACTION = "STATUS_TRANSACTION";
+}
