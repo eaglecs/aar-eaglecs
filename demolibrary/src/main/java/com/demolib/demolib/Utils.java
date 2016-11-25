d23f0691b2ab4028b8eba32d593fe839d6fa1d87
leducanh <leducanh.bkit10@gmail.com>
2016-11-25 10:04:30 +0700

    update README

---
+++ demolibrary/src/main/java/com/demolib/demolib/Utils.java
@@ -0,0 +1,30 @@
+package com.demolib.demolib;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by duong_000 on 11/4/2016.
+ */
+
+public class Utils {
+
+    public static void saveToPrefs(Context context, String key, String value) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        final SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public static String getFromPrefs(Context context, String key, String defaultValue) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        try {
+            return sharedPrefs.getString(key, defaultValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return defaultValue;
+        }
+    }
+
+}
